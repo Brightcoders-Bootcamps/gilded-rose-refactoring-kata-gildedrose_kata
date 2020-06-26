e3c64cfab4f95f3d04d75f3a7e1d276900df5dee
IscJacobo19 <iscjacobomanuel@gmail.com>
2020-06-26 18:56:43 -0500

    I added a test of Aged Brie we add a test in the gild_rose_spec.rb file on line 13

--- spec/gilded_rose_spec.rb
+++ spec/gilded_rose_spec.rb
@@ -10,6 +10,10 @@ describe GildedRose do
       GildedRose.new(items).update_quality()
       items[0].name.should == "fixme"
     end
+    it "test item Aged Brie" do 
+      items = [Item.new("Aged Brie", 14, 10), Item.new("Aged Brie", 9, 7)]
+      expect(GildedRose.new(items).update_quality().each{|item| item.to_s()}).to eq("Aged Brie, 13, 11")
+    end
   end
 
 end
